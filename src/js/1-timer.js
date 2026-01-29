a306485f7bfc17813ab8e7a939ff80c575895925
max-shakula <maxshakulakr@gmail.com>
2026-01-29 22:19:05 +0200

    fix2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,46 +4,22 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 let selectedDate;
-document.querySelector('[data-start]').disabled = true;
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-//   clickOpens: true,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    selectedDate = selectedDates[0];
-    const currentDate = new Date();
-    if (selectedDate <= currentDate) {
-      iziToast.error({
-        title: 'Error',
-        message: 'Please choose a date in the future!',
-        position: 'topRight',
-      });
-    } else {
-      document.querySelector('[data-start]').disabled = false;
-    }
-  },
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#datetime-picker');
-
-  flatpickr(input, options);
-});
-
+const input = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
+startBtn.disabled = true;
+
+
 startBtn.addEventListener('click', () => {
   console.log('Start timer');
+  startBtn.disabled = true;
+  input.disabled = true;
   const timerHandler = setInterval(() => {
     const currentDate = new Date();
     const deltaTime = selectedDate - currentDate;
     if (deltaTime <= 0) {
       clearInterval(timerHandler);
-      document.querySelector('[data-start]').disabled = false;
-      document.querySelector('#datetime-picker').disabled = false;
+      startBtn.disabled = false;
+      input.disabled = false;
       iziToast.info({
         title: 'Time is up!',
         message: 'Countdown is finished.',
@@ -55,12 +31,40 @@ startBtn.addEventListener('click', () => {
 
     document.querySelector('[data-days]').textContent = addLeadingZero(days);
     document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
-    document.querySelector('[data-minutes]').textContent = addLeadingZero(minutes);
-    document.querySelector('[data-seconds]').textContent = addLeadingZero(seconds);
+    document.querySelector('[data-minutes]').textContent =
+      addLeadingZero(minutes);
+    document.querySelector('[data-seconds]').textContent =
+      addLeadingZero(seconds);
+  }, 1000);
+});
 
-    document.querySelector('[data-start]').disabled = true;
-    document.querySelector('#datetime-picker').disabled = true;
-  })
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    const currentDate = new Date();
+    if (selectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future!',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const inputValue = document.querySelector('#datetime-picker');
+
+  flatpickr(inputValue, options);
 });
 
 
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,39 +1,38 @@
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('.form');
 form.addEventListener('submit', onFormSubmit);
 
-function onFormSubmit(event) {
-    event.preventDefault();
-
-    const delay = Number(form.delay.value) || 0;
-    const state = form.elements.state.value;
-
-    console.log(delay, state);
-    
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        const promise = new Promise((resolve, reject) => {
-            if (state === 'fulfilled') {
-                resolve('Success');
-                iziToast.success({
-                    title: 'Fulfilled!',
-                    message: `Fulfilled after ${delay}ms`,
-                    position: 'topRight',
-                });
-            } else if (state === 'rejected') {
-                reject('Error');
-                iziToast.error({
-                    title: 'Rejected!',
-                    message: `Rejected after ${delay}ms`,
-                    position: 'topRight',
-                });
-            } else {
-                iziToast.warning({
-                    title: 'Sorry!',
-                    message: 'Something went wrong...',
-                    position: 'topRight',
-                }) // ! For warning to work change state value in line 23 or 16
-            }
-        });
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
     }, delay);
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const delay = Number(form.delay.value);
+  const state = form.elements.state.value;
+
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
 }
